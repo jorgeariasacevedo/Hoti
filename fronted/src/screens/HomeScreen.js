b8227e294132443fc00093d9657396a4b32e9e06
Jorge <jorgearias-2008@hotmail.com>
2021-02-15 01:13:56 -0500

    2:41

--- fronted/src/screens/HomeScreen.js
+++ fronted/src/screens/HomeScreen.js
@@ -1,28 +1,18 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect } from "react";
 import Product from "../componentes/Product";
 import LoadingBox from "../componentes/LoadingBox";
 import MessageBox from "../componentes/MessageBox";
+import { useDispatch, useSelector } from "react-redux";
+import { listProducts } from "../actions/productActions";
 export default function Homescreen() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   /*Esto pasa la info del API al fronted y muestra los errores */
   useEffect(() => {
-    const fecthData = async () => {
-      try {
-        setLoading(true);
-        const { data } = await axios.get("/api/products");
-        setLoading(false);
-        setProducts(data);
-      } catch (err) {
-        setError(err.message);
-        setLoading(false);
-      }
-    };
-    fecthData();
-  }, []);
+    dispatch(listProducts());
+  }, [dispatch]);
   return (
     <div>
       {" "}
